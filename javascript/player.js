213089f1d8d7f8169a399b78d858177a371cdb18
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-18 10:05:12 +0100

    Add player move functions

---
+++ javascript/player.js
@@ -0,0 +1,30 @@
+class Player extends Template {
+
+    constructor(x,y,w,h,color) {
+        super(x,y,w,h,color);
+        this.speed = 10;
+    }
+
+    update() {
+        this.move();
+    }
+
+    moveUp() {
+        if(this.y <= 25) {this.y = 0}
+        this.y -= this.speed;
+    }
+    moveDown() {
+        if(this.y >= canvas.height - 25) {this.y = 0}
+        this.y += this.speed;
+    }
+    moveLeft() {
+        if(this.x <= 25) {this.x = 0}
+        this.x -= this.speed;
+    }
+    moveRight() {
+        if(this.x >= canvas.width - 25) {this.x = 0}
+        this.x += this.speed;
+    }
+
+
+}
