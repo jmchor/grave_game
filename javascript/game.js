26a01e3b7e50a91623435196a60a78d6383fe3b3
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-19 21:04:54 +0100

    Add hasDoorKey variable, shift code order

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,10 @@
 
-let player = new Player(50, 700, 40, 40, 'brown');
-let key = new Template(1100, 700, 20, 40, 'blue')
+let player = new Player(50, 650, 40, 40, 'brown');
+let key = new Template(1100, 650, 20, 40, 'blue')
+let door = new Template(1100, 50, 40, 80, 'green')
+
+let keyPlace = [key]
+let inventory = []
 
 const graveSite = {
 
@@ -11,16 +15,40 @@ const graveSite = {
     isRightKeyPressed: false,
     isUpKeyPressed: false,
     isDownKeyPressed: false,
+    hasDoorKey: false,
 
     startGame: function() {
         graveSite.isGameOver = false;
         graveSite.isGamePaused = false;
 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawMap()
         player.draw();
-        key.draw();
+        door.draw()
+
+        if (keyPlace.includes(key)) {
+            key.draw()
+        }
+
     },
 
     update: function() {
+
+        if (graveSite.isGamePaused) return
+
+
+        walls.forEach(wall => {
+            if (player.detectCollision(wall)) {
+                let previousY = player.y - player.velocity.y;
+                let previousX = player.x - player.velocity.x;
+                player.velocity.x = 0;
+                player.velocity.y = 0;
+                player.x = previousX
+                player.y = previousY;
+
+            };
+        })
+
 
         if (graveSite.isLeftKeyPressed) {
             player.moveLeft();
@@ -34,36 +62,17 @@ const graveSite = {
         if (graveSite.isDownKeyPressed) {
             player.moveDown();
         }
-
         player.update();
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawMap()
         player.draw();
-        key.draw()
+        door.draw()
 
-        walls.forEach(wall => {
-            if (player.detectCollision(wall)) {
-                let previousY = player.y - player.velocity.y;
-                let previousX = player.x - player.velocity.x;
-                player.velocity.x = 0;
-                player.velocity.y = 0;
-                player.x = previousX
-                player.y = previousY;
-
-                this.isLeftKeyPressed = false;
-                this.isRightKeyPressed = false;
-                this.isUpKeyPressed = false;
-                this.isDownKeyPressed = false;
-
-            };
-
-    })
-
-    },
+       getKey()
 
 
-
+    }
 }
 
 
