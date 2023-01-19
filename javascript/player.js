c9b40a6e39488e166b7184c2f6b805ad35fd5fb3
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-19 21:05:43 +0100

    Add doorCollision, getKey functions

--- javascript/player.js
+++ javascript/player.js
@@ -17,19 +17,19 @@ class Player extends Template {
     }
 
     moveUp() {
-        this.velocity.y = -5;
+        this.velocity.y = -10;
     }
     moveDown() {
-        this.velocity.y = 5;
+        this.velocity.y = 10;
 
     }
     moveLeft() {
-       this.velocity.x = -5;
+       this.velocity.x = -10;
 
     }
     moveRight() {
 
-        this.velocity.x = 5;
+        this.velocity.x = 10;
 
     }
 
@@ -40,8 +40,57 @@ class Player extends Template {
             this.x + this.velocity.x <= target.x + target.w)
             {
                 return true
+            };
+        };
+
+    doorCollision(target) {
+        if (this.y  <= target.y + target.h  &&
+            this.x + this.w >= target.x &&
+            this.y + this.h >= target.y &&
+            this.x  <= target.x + target.w)
+            {
+                return true
+            };
+        };
+
+
 
     }
 
-}
-}
+
+
+    function getKey (){
+        if (player.detectCollision(key)) {
+            graveSite.hasDoorKey = true;
+        }
+        if (keyPlace.includes(key)) {
+            key.draw()
+        }
+        if(graveSite.hasDoorKey) {
+            key.clear()
+            keyPlace.pop()
+            inventory.push(key)
+        }
+        if (inventory.includes(key)) {
+            let inventoryKey = new Template (1100, 750, 20, 40, 'blue')
+            inventoryKey.draw()
+
+        }
+
+    }
+
+
+    function openDoor() {
+
+        if (player.doorCollision(door) && graveSite.hasDoorKey) {
+            graveSite.isGameOver = true;
+            graveSite.isGamePaused = true;
+            ctx.clearRect(0,0,canvas.width, canvas.height)
+            ctx.fillStyle = 'black'
+            ctx.fillRect(0,0,canvas.width, canvas.height,)
+            ctx.font = '50px Arial'
+            ctx.fillStyle = 'white'
+            ctx.fillText('You Win!', 500, 400)
+
+        }
+    }
