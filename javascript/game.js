dbebdf089ad33f470c546a70f25cd8a3750e436c
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-19 14:07:43 +0100

    Add wall collision detection

--- javascript/game.js
+++ javascript/game.js
@@ -33,11 +33,30 @@ const graveSite = {
             player.moveDown();
         }
 
-        player.updateMovement();
+        player.update();
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawMap()
         player.draw();
+
+        walls.forEach(wall => {
+            if (player.detectCollision(wall)) {
+                let previousY = player.y - player.velocity.y;
+                let previousX = player.x - player.velocity.x;
+                player.velocity.x = 0;
+                player.velocity.y = 0;
+                player.x = previousX
+                player.y = previousY;
+
+                this.isLeftKeyPressed = false;
+                this.isRightKeyPressed = false;
+                this.isUpKeyPressed = false;
+                this.isDownKeyPressed = false;
+
+            };
+
+    })
+
     },
 
 
