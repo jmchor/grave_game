03c3b35e1e73e30b60f7ae38238f897d113c9977
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-21 13:00:39 +0100

    Add ghoul & encounter function

--- javascript/game.js
+++ javascript/game.js
@@ -3,7 +3,11 @@ let player = new Player(50, 620, 50, 50, 'brown');
 let key = new Key(1100, 650, 20, 40, 'img/key-blue.png')
 let door = new Template(1100, 50, 40, 80, 'green')
 let currentTime = 0;
-let check1 = new Template (300, 300, 5, 5, 'black')
+let check1 = new Template (400, 300, 10, 10, 'black')
+let check2 = new Template (400, 530, 10, 10, 'black')
+let check3 = new Template (800, 530, 10, 10, 'black')
+let check4 = new Template (800, 300, 10, 10, 'black')
+let ghoul = new Ghoul(400, 300, 50, 50, 'pink')
 
 
 
@@ -32,8 +36,9 @@ const graveSite = {
 
         drawMap()
         player.draw();
+        ghoul.draw();
         door.draw()
-        check1.draw()
+
         graveSite.displayTime()
 
         if (keyPlace.includes(key)) {
@@ -63,7 +68,7 @@ const graveSite = {
 
     update: function() {
 
-
+        encounterEnemy()
         walls = []
 
         if (graveSite.isGamePaused) return
@@ -84,6 +89,8 @@ const graveSite = {
             player.moveDown();
         }
         player.update();
+        ghoul.update();
+
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawMap()
@@ -100,8 +107,9 @@ const graveSite = {
         })
 
         player.draw();
+        ghoul.draw();
         door.draw()
-        check1.draw()
+
 
         graveSite.displayTime()
 
