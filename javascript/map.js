32001a07943496f40bb0cde6daa44bf969b73a32
jc <jchorzempa@posteo.org>
2023-01-24 08:50:45 +0100

    Move sound()

--- javascript/map.js
+++ javascript/map.js
@@ -236,21 +236,7 @@ function drawMap(){
 }
 
 
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.playbackRate = 4;
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-      this.sound.play();
-    }
-    this.stop = function(){
-      this.sound.pause();
-    }
-  }
+
 
 
 
